db62b40c5e4575baf6c71b07f216bee486f6b359
Nishant <nishantb1130@gmail.com>
2022-02-12 23:30:41 -0800

    backend stuffs

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,23 @@
 from flask import Flask
+import spoiler_value from 'utils.py'
 
 app = Flask(__name__)
 
 # will have a json obj as param
 @app.route('/create')
-def create_post():
-    return "post created"    
+def create_post(data):
+    val = spoiler_value(data.description)
+    if val >= 0.3:
+        val['spoiler'] = True
+    return "post created"
 
 # will have a json obj as param
 @app.route('/comment')
-def comment_post():
+def comment_post(data):
+    val = spoiler_value(data.description)
+    if val >= 0.3:
+        val['spoiler'] = True
     return "comment created"
-
-def spoiler_val():
-    result = request.form.to_dict(flat=True)
-    sentence = result.get("sentence")
-    spoil_val = utils.spoiler_value(sentence)
-    result["spoil_val"] = spoil_val
-    return render_template("index.html", result=result)
-
-
-
 
 @app.route('/')
 def hello_world():
