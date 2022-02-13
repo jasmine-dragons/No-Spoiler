ab628bbb316b6f548c786bf9b48a0fe6e018a088
Christopher Cha <ccha@ucsd.edu>
2022-02-13 01:24:59 -0800

    mongodb edits to app

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,22 @@
-from flask import Flask
-import spoiler_value from 'utils.py'
-
+from flask import Flask 
+from utils import spoiler_value
 app = Flask(__name__)
+
+db = g._database = PyMongo(current_app).db
 
 # will have a json obj as param
 @app.route('/create')
 def create_post(data):
+    '''
+    create_post(data)
+    data has username, title, description, list of comments
+    '''
     val = spoiler_value(data.description)
     if val >= 0.3:
         val['spoiler'] = True
-    return "post created"
+    post_doc = {'username' : data.username, 'description' : data.description, 'comments' : data.comments}
+    return db.comments.insert_one(post_doc)
+    # return "post created"
 
 # will have a json obj as param
 @app.route('/comment')
@@ -17,7 +24,9 @@ def comment_post(data):
     val = spoiler_value(data.description)
     if val >= 0.3:
         val['spoiler'] = True
-    return "comment created"
+    comment_doc = {'username' : data.username, 'comment' : data.comment, 'spoiler' : val['spoiler']}
+    return db.comments.insert_one(comment_doc)
+    # return "comment created"
 
 @app.route('/')
 def hello_world():
