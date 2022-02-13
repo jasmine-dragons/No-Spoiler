643c9fac69331901497694227975f150e3c2fa53
Nishant <nishantb1130@gmail.com>
2022-02-12 20:14:55 -0800

    start routes

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# will have a json obj as param
+@app.route('/create')
+def create_post():
+    return "post created"    
+
+# will have a json obj as param
+@app.route('/comment')
+def comment_post():
+    return "comment created"
+
+
+
+
+
 @app.route('/')
 def hello_world():
     return "<p>Hello, World!</p>"
