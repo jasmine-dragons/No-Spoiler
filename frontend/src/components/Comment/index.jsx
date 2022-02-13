1c71228571e0abf320f1feb322aa9a91cd5320c6
sq19 <shqi@ucsd.edu>
2022-02-13 07:52:29 -0800

    added spoiler clicking

--- frontend/src/components/Comment/index.jsx
+++ frontend/src/components/Comment/index.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import "./style.less";
 
 const Comment = ({username, comment, spoiler}) => {
+    const [isSpoiler, setSpoiler] = useState(spoiler);
     return (
         <div className="comment">
             <p>{username}</p>
-            <h2 className={`content ${spoiler ? "spoiler" : ""}`}>{comment}</h2>
+            <h2 className={`content ${spoiler ? "spoiler" : ""}`} onClick={() => setSpoiler(false)}>{comment}</h2>
         </div>
     );
 };
--- frontend/src/components/LargePost/index.jsx
+++ frontend/src/components/LargePost/index.jsx
@@ -1,11 +1,14 @@
 import "./style.less"
+import { useState } from "react";
 
 const LargePost = ({username, title, body, spoiler}) => {
+    const [isSpoiler, setSpoiler] = useState(spoiler);
+
     return(
         <div className="post-body">
             <p>{username}</p>
             <h1>{title}</h1>
-            <h2 className={`content ${spoiler ? "spoiler" : ""}`}>{body}</h2>
+            <h2 className={`content ${spoiler ? "spoiler" : ""}`} onClick={() => setSpoiler(false)}>{body}</h2>
         </div>
     );
 };
