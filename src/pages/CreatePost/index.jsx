b02e63cf84db744e9f89df84140e10cab8bc9d9a
sq19 <shqi@ucsd.edu>
2022-02-13 00:05:55 -0800

    created post request for submitting post

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,7 +1,28 @@
 import "./style.less";
 import logo from '../../assets/logo.svg';
+import { useState } from "react";
 
 const CreatePost = () => {
+    const [username, setUsername] = useState("");
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const submitPost = () => {
+        fetch('https://localhost/5000/create', {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: username,
+                title: title,
+                content: content
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
   	return (
 		<div className="CreatePost">
 			<header className="CreatePost-header">
@@ -10,15 +31,15 @@ const CreatePost = () => {
 			</header>
 			<div className="CreatePost-body">
 				<div className="post">
-					<p>username</p>
-					<input className="title" type="text" placeholder="Put Title Here" />
+					<p>{username}</p>
+					<input className="title" type="text" placeholder="Put Title Here" value={title} onChange={e => setTitle(e.target.value)}/>
 					<div className="blank"></div>
-					<textarea className="content" placeholder="Body of Content" />
+					<textarea className="content" placeholder="Body of Content" value={content} onChange={e => setContent(e.target.value)}/>
 				</div>
 
 				<div className="buttons">
 					<button>Cancel</button>
-					<button>Submit</button>
+					<button onClick={submitPost}>Submit</button>
 				</div>
 			</div>
 		</div>
